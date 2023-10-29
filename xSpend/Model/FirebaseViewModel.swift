3f23c3d522706044318db5b379bcaa3c5a5c5fdc
Mike Paraskevopoulos <mikeparaskevopoulos.mp@gmail.com>
2023-10-29 18:43:05 +0200

    architecture chage

---
+++ xSpend/Model/FirebaseViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FirebaseViewModel.swift
+//  xSpend
+//
+//  Created by Mike Paraskevopoulos on 29/10/23.
+//
+
+import SwiftUI
+
+struct FirebaseViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirebaseViewModel()
+}
