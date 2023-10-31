c7e47667473244a2eb061e42c81002de5f85bb0b
Mike Paraskevopoulos <mikeparaskevopoulos.mp@gmail.com>
2023-10-31 02:27:51 +0200

    get structured expenses sorted and groupted by date

---
+++ xSpend/View/ExpensesScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ExpensesScreen.swift
+//  xSpend
+//
+//  Created by Mike Paraskevopoulos on 30/10/23.
+//
+
+import SwiftUI
+
+struct ExpensesScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpensesScreen()
+}
