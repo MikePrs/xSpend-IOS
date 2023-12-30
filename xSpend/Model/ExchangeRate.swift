6acf592fd95f0319b5cba448ffd2ed19e4cefcbe
Mike Paraskevopoulos <mikeparaskevopoulos.mp@gmail.com>
2023-12-31 00:05:33 +0200

    exchange rates logic + api

---
+++ xSpend/Model/ExchangeRate.swift
@@ -0,0 +1,8 @@
+//
+//  ExchangeRate.swift
+//  xSpend
+//
+//  Created by Mike Paraskevopoulos on 30/12/23.
+//
+
+import Foundation
---
+++ xSpend/Model/ExchangeRatesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ExchangeRatesViewModel.swift
+//  xSpend
+//
+//  Created by Mike Paraskevopoulos on 30/12/23.
+//
+
+import Foundation
